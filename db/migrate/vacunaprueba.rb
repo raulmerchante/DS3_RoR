c18216980f910709bac5d260fe165d215734a5c1
Miguel Reyes Martínez <60162079+miguelreyesmr@users.noreply.github.com>
2021-05-24 11:08:09 +0200

    Update vacunaprueba.rb

--- db/migrate/vacunaprueba.rb
+++ db/migrate/vacunaprueba.rb
@@ -1,7 +1,6 @@
 class CreateVacunaprueba < ActiveRecord::Migration[6.1]
   def change
     create_table :vacunaprueba do |t|
-      t.integer :id
       t.integer :antigenos
       t.integer :lipidos
       t.integer :conservantes
