5bf6c100c467efc3a58b34e6302d84a12618483b
Miguel Reyes Martínez <60162079+miguelreyesmr@users.noreply.github.com>
2021-05-24 10:53:35 +0200

    prueba3

---
+++ db/migrate/vacunaprueba.rb
@@ -0,0 +1,13 @@
+class CreateVacunaprueba < ActiveRecord::Migration[6.1]
+  def change
+    create_table :vacunaprueba do |t|
+      t.integer :id
+      t.integer :antigenos
+      t.integer :lipidos
+      t.integer :conservantes
+
+      t.timestamps
+    end
+    add_index :vacunaprueba, :id, unique: true
+  end
+end
