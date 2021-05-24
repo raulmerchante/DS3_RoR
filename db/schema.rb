17aecfccb93c272142f10e36e904df95ea663b6a
raulmerchante <raulmerchante@gmail.com>
2021-05-24 11:14:10 +0200

    prueba_terminal2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_155325) do
+ActiveRecord::Schema.define(version: 2021_05_24_085536) do
 
   create_table "developers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_05_14_155325) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["team"], name: "index_projects_on_team", unique: true
+  end
+
+  create_table "prueba_vacunas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "antigenos"
+    t.integer "lipidos"
+    t.integer "conservantes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "developers", "projects"
