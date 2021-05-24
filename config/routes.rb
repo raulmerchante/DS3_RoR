62d37fb2e1fe163449eb34a8f7fd864fe0bf5f2f
Miguel Reyes Martínez <60162079+miguelreyesmr@users.noreply.github.com>
2021-05-24 13:12:43 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :mivacunas
   resources :developers
   resources :projects
-  root "mivacunas#index"
+  root "developers#index"
 
   namespace :api, defaults: { format: 'json' }  do
     namespace :v1 do
