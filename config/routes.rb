85aca2ed9f88f8ef634b4d62ac344e56802c9dfa
Miguel Reyes Martínez <60162079+miguelreyesmr@users.noreply.github.com>
2021-06-02 15:36:03 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' }  do
     namespace :v1 do
-      resources :projects, :mivacunas, :diarios
+      resources :projects, :mivacunas, :diarios, :desechos
     end
   end
   
