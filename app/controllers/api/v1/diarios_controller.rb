417c43cdeca2ac3cbf660af3439b78a6e599037c
Miguel Reyes Martínez <60162079+miguelreyesmr@users.noreply.github.com>
2021-06-02 12:47:16 +0200

    Create diarios_controller.rb

---
+++ app/controllers/api/v1/diarios_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+    class DiariosController < ApplicationController
+  
+  skip_before_action :verify_authenticity_token
+  
+  def create
+    @diario = Diario.new(project_params)
+
+      if @diario.save
+        render json: @diario, status: :created
+      else
+        render json: @diario.errors, status: :unprocessable_entity
+      end
+end
+  
+  def index
+          render json: Diario.all, status: :ok
+  end
+
+
+  private
+  def project_params
+        params.require(:diario).permit(:nombre_paciente, :apellidos_paciente, :fecha, :dni)
+  end
+  end
+  end
+  end
+  
