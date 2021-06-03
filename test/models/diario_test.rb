00e2be376ec87a08548587b1fd98c672d5f9900a
Raul Merchante <raulmerchante@gmail.com>
2021-06-03 20:40:51 +0200

    Update diario_test.rb

--- test/models/diario_test.rb
+++ test/models/diario_test.rb
@@ -7,6 +7,6 @@ class DiarioTest < ActiveSupport::TestCase
 
   test ".word_count returns the correct number of words" do 
     diario = Diario.new(apellidos_paciente: "Garcia de Dios") 
-    assert_equal 3, diario.word_count
+    assert_equal 3, diario.apellidos_paciente.word_count
   end
 end
