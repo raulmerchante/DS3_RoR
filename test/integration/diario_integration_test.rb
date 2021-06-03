334f259583231753397d05ef087b19e010a18778
Raul Merchante <raulmerchante@gmail.com>
2021-06-03 20:47:29 +0200

    Update diario_integration_test.rb

--- test/integration/diario_integration_test.rb
+++ test/integration/diario_integration_test.rb
@@ -1,6 +1,6 @@
 class DiarioTest < ActionDispatch::IntegrationTest 
     test "can see the welcome page" do
         get "/"
-        assert_select "h1", "VacunaT#index" 
+        assert_select "h1", "VacunaT" 
     end
 end
