aa4793e073a82dab6e3f0c94fd49a0c4896016e6
raulmerchante <raulmerchante@gmail.com>
2021-06-03 20:17:30 +0200

    prueba

--- app/models/prueba_vacuna.rb
+++
@@ -1,2 +0,0 @@
-class PruebaVacuna < ApplicationRecord
-end
--- db/migrate/20210524091910_create_prueba_rauls.rb
+++
@@ -1,9 +0,0 @@
-class CreatePruebaRauls < ActiveRecord::Migration[6.1]
-  def change
-    create_table :prueba_rauls do |t|
-      t.integer :pruebaedad
-
-      t.timestamps
-    end
-  end
-end
